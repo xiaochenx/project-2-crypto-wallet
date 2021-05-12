3be236e9bba4f59f528ba420f522de171958c1c2
Xiaochen Xu <xiaochex@gmail.com>
2021-05-11 20:02:15 -0400

    updated map with filter method in Watchlist component

--- crypto-wallet/src/components/Watchlist.js
+++ crypto-wallet/src/components/Watchlist.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button'
 const Watchlist = ({cryptos , setCryptos }) => {
   
     const handleClick = (id) => (e) => {
+        console.log(e.target.value)
         const newCryptos = cryptos.map(el => { 
             if(el.id === id){
              return ({...el , watchList : false})
@@ -14,18 +15,15 @@ const Watchlist = ({cryptos , setCryptos }) => {
         setCryptos(newCryptos);
      }
 
-    const renderCryptos = cryptos.filter(crypto =>{
-        
-        if(!crypto.watchList) return null;
-        return <div>
-            <h1>{crypto.name}</h1> <Button variant="outline-danger" onClick={handleClick(crypto.id)}>Delete</Button>
-            <h2>Price: ${crypto.price} (usd)</h2> 
-            <h3>Market Cap: ${crypto.marketCap}</h3> 
-            <h3>24 Hour trend: {crypto.ChangePercent24Hr}%</h3>
+    const renderCryptos = cryptos.filter(crypto =>crypto.watchList === true).map(newCrypto => (
+         <div>
+            <h1>{newCrypto.name}</h1> <Button variant="outline-danger" onClick={handleClick(newCrypto.id)}>Delete</Button>
+            <h2>Price: ${newCrypto.price} (usd)</h2> 
+            <h3>Market Cap: ${newCrypto.marketCap}</h3> 
+            <h3>24 Hour trend: {newCrypto.ChangePercent24Hr}%</h3>
         <hr />
         </div>
-    }
-
+    )
         );
 
     
@@ -43,3 +41,6 @@ Watchlist.defaultProps = {
 
 
 export default Watchlist 
+
+
+
